3c13705f75b6b2b5fca1c131f0b5a367f61852f3
Blaster1111 <rudrashukla0711@gmail.com>
2024-09-25 22:17:31 +0530

    task 2 service logic modified

--- services/tradeService.js
+++ services/tradeService.js
@@ -5,38 +5,25 @@ const storeTrades = async (filePath) => {
     const trades = await parseCSV(filePath);
     await Trade.insertMany(trades);
 };
+
 const getBalanceAtTimestamp = async (timestamp) => {
     const date = new Date(timestamp);
     const trades = await Trade.find({ utcTime: { $lte: date } });
-    let btcBalance = 0;
-    let maticBalance = 0;
 
-    console.log('Trades before timestamp:', trades);
-    trades.forEach((trade) => {
-        const asset = trade.baseCoin; 
-        if (trade.operation === 'Buy') {
-            if (asset === 'BTC') {
-                btcBalance += trade.amount; 
-                console.log(btcBalance);
-            } else if (asset === 'MATIC') {
-                maticBalance += trade.amount;
-                console.log(maticBalance); 
-            }
-        } else if (trade.operation === 'Sell') {
-            if (asset === 'BTC') {
-                    btcBalance -= trade.amount;
-                    console.log(btcBalance);
-            } else if (asset === 'MATIC') {
-                    maticBalance -= trade.amount;
-                    console.log(maticBalance);
-               
-            }
-        } 
-    });
-    return {
-        BTC: btcBalance,
-        MATIC: maticBalance,
+    const initialBalances = {
+        BTC: 0,
+        MATIC: 0,
     };
-};
+    const balances = trades.reduce((acc, trade) => {
+        const asset = trade.baseCoin;
 
+        if (trade.operation === 'Buy') {
+            acc[asset] += trade.amount;
+        } else if (trade.operation === 'Sell') {
+            acc[asset] -= trade.amount; 
+        }
+        return acc;
+    }, initialBalances);
+    return balances; 
+};
 export { storeTrades, getBalanceAtTimestamp };
